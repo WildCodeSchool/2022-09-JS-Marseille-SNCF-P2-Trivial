0a236e4bc43cd6a9308173305a2aeb0df3f3b2a2
fredl2tp <fleschira@gmail.com>
2022-12-07 16:04:20 +0100

    first feature-content

--- react-trivial/src/App.js
+++ react-trivial/src/App.js
@@ -1,19 +1,18 @@
-import React, {useState} from 'react';
-import Header from './Components/Header';
-import Nav from './Components/Nav';
-import Container from './Components/Container';
-import Footer from './Components/Footer';
-import './App.css';
-import axios from './axios';
-
+import React, { useState } from "react";
+// import Header from './Components/Header';
+// import Nav from './Components/Nav';
+import Content from "./Components/Content";
+// import Footer from './Components/Footer';
+import "./App.css";
+import axios from "axios";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Nav/>
-      <Container/>
-      <Footer/>
+      {/* <Header />
+      <Nav /> */}
+      <Content />
+      {/* <Footer /> */}
     </div>
   );
 }
--- react-trivial/src/Components/Content.jsx
+++ react-trivial/src/Components/Content.jsx
@@ -1,3 +1,88 @@
 import React from "react";
+import image1 from "../Images/image1.jpg";
+import "./Content.css";
 
-export default Container;
+const Content = () => {
+  return (
+    <div className="content">
+      <h1>Chose a category</h1>
+      <div className="card-list">
+        <div className="cards">
+          <article className="card">
+            <img src={image1} alt="catégorie1" />
+            <div className="container">
+              <h2>
+                <b>Sports</b>
+              </h2>
+            </div>
+          </article>
+          <article className="card">
+            <img src={image1} alt="catégorie2" />
+            <div className="container">
+              <h2>
+                <b>Musique</b>
+              </h2>
+            </div>
+          </article>
+          <article className="card">
+            <img src={image1} alt="catégorie3" />
+            <div className="container">
+              <h2>
+                <b>Games videos</b>
+              </h2>
+            </div>
+          </article>
+          <article className="card">
+            <img src={image1} alt="catégorie4" />
+            <div className="container">
+              <h2>
+                <b>Geographie</b>
+              </h2>
+            </div>
+          </article>
+          <article className="card">
+            <img src={image1} alt="catégorie5" />
+            <div className="container">
+              <h2>
+                <b>Film/TV</b>
+              </h2>
+            </div>
+          </article>
+          <article className="card">
+            <img src={image1} alt="catégorie6" />
+            <div className="container">
+              <h2>
+                <b>Manga</b>
+              </h2>
+            </div>
+          </article>
+          <article className="card">
+            <img src={image1} alt="catégorie7" />
+            <div className="container">
+              <h2>
+                <b>Mythologie</b>
+              </h2>
+            </div>
+          </article>
+          <article className="card">
+            <img src={image1} alt="catégorie8" />
+            <div className="container">
+              <h2>
+                <b>Choix Aleatoire</b>
+              </h2>
+            </div>
+          </article>
+          <article className="card">
+            <img src={image1} alt="catégorie9" />
+            <div className="container">
+              <h2>
+                <b>All categorys</b>
+              </h2>
+            </div>
+          </article>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Content;
