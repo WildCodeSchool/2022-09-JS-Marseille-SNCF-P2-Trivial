e84cd68ac5922b91e801ccd58beece9d14b67979
Samikate <samikate@hotmail.fr>
2022-12-15 10:05:03 +0100

    Container component deleted

--- react-trivial/src/App.js
+++ react-trivial/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Header from './Components/Header';
 import Nav from './Components/Nav';
 import Game from './Components/Game';
-//import Container from './Components/Container';
 import Footer from './Components/Footer';
 import './App.css';
 
