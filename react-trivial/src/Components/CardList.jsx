d124e159a6246e0194f65542a4e666424393a267
fredl2tp <fleschira@gmail.com>
2022-12-16 08:37:28 +0100

    id

--- react-trivial/src/Components/CardList.jsx
+++ react-trivial/src/Components/CardList.jsx
@@ -4,18 +4,16 @@ import imageAnimal from "../Images/imageAnimal.jpg";
 
 const CardList = ({ name, id }) => {
   return (
-    <div className={id}>
-    <div className="card">
-      {/* <a href="test" target="_blank"> */}
+    // <div className={key}>
+    <div key={id} className="card">
       <div className="categoryImg">
         <g-img>
           <img src={imageAnimal} alt="" />
         </g-img>
       </div>
       <div className="categoryText">{name}</div>
-      {/* </a> */}
     </div>
-    </div>
+    // </div>
   );
 };
 
