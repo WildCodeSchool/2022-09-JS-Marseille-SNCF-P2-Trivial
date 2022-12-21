5dd43ec647b3cbf5b529a4633711a67691da84ee
fredl2tp <fleschira@gmail.com>
2022-12-21 10:59:03 +0100

    just before prez

--- react-trivial/src/Components/Content.jsx
+++ react-trivial/src/Components/Content.jsx
@@ -2,21 +2,14 @@ import React from "react";
 import "./Content.css";
 import Card from "./Card";
 
+
 const Content = ({ setBegin }) => {
-  // const [begin, setBegin] = useState(false);props={setBegin}
-  const beginGame = () => setBegin(true);
-  console.log("test : ", setBegin);
   return (
     <div className="content">
+      {/* property and parameter for switching */}
       <Card setBegin={setBegin} />
-      <button onClick={beginGame}>Begin</button>;
     </div>
   );
 };
-
-// render() {
-//   if (this.state.edit) return <EditForm />
-//   return <Display />
-// }
 
 export default Content;
